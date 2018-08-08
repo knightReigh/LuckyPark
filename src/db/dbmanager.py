cbd0c55b734664f42c5c007919330c2f7192ece1
knightReigh <12376953+knightReigh@users.noreply.github.com>
2018-08-08 12:19:06 -0500

    start bulding db api

---
+++ src/db/dbmanager.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+DB_HOST_ADDRESS = "mongodb://localhost:27071"
+
+class park_dbmanager():
+    client = MongoClient()
+
+    # insert list
+
+    # insert park info
+
+    # remove
+
+    # find
+
+    # sort
+
+    # list
+
+
+
